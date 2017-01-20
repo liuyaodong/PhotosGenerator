9041a2edb5abe9914e455a300c207baa604e11a5
liuyaodong <liuyaodong@bytedance.com>
2017-01-20 11:26:12 +0800

    支持takenDate生成

--- PhotosGenerator/PhotosGeneratingViewController.m
+++ PhotosGenerator/PhotosGeneratingViewController.m
@@ -24,10 +24,11 @@
     NSInteger           _count;
     dispatch_queue_t    _drawingQueue;
     NSOperation         *_operation;
+    NSDateFormatter     *_formatter;
 }
 
 - (void)dealloc {
-    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)viewDidLoad {
@@ -40,6 +41,9 @@
     
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped:)];
     [self.view addGestureRecognizer:tap];
+    
+    _formatter = [NSDateFormatter new];
+    _formatter.dateFormat = @"yyyy:MM:dd HH:mm:ss";
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -48,9 +52,15 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSUInteger count = [PHAsset fetchAssetsWithOptions:nil].count;
         dispatch_async(dispatch_get_main_queue(), ^{
-            self.mediaCountLabel.text = [NSString stringWithFormat:@"当前media: %lu", count];
+            self.mediaCountLabel.text = [NSString stringWithFormat:@"当前media: %lu", (unsigned long)count];
         });
     });
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [_operation cancel];
 }
 
 - (void)tapped:(id)sender
@@ -71,8 +81,8 @@
     if (_isGenerating) {
         self.textField.enabled = NO;
         _operation = [self generateImageWithCount:_count progress:^(NSUInteger completed, NSUInteger failed) {
-            self.progressLabel.text = [NSString stringWithFormat:@"已经生成：%lu/%ld", completed, _count];
-            self.errorLabel.text = [NSString stringWithFormat:@"出错：%lu", failed];
+            self.progressLabel.text = [NSString stringWithFormat:@"已经生成：%lu/%ld", (unsigned long)completed, (long)_count];
+            self.errorLabel.text = [NSString stringWithFormat:@"出错：%lu", (unsigned long)failed];
         } completion:^{
             _isGenerating = NO;
             self.textField.enabled = YES;
@@ -84,14 +94,33 @@
     }
 }
 
+- (NSURL *)replaceTakenDateOfImage:(UIImage *)image withDate:(NSDate *)date
+{
+    NSString *dateString = [_formatter stringFromDate:date];
+    NSDictionary *imageMeta = @{(NSString *)kCGImagePropertyExifDictionary:
+                                    @{
+                                        (NSString *)kCGImagePropertyExifDateTimeOriginal: dateString,
+                                        (NSString *)kCGImagePropertyExifDateTimeDigitized: dateString
+                                        }
+                                };
+    NSData *imageData = UIImagePNGRepresentation(image);
+    CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef)imageData, NULL);
+    CFStringRef UTI = CGImageSourceGetType(source);
+    NSURL *tmpURL = [[[[[NSFileManager defaultManager] URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] firstObject] URLByAppendingPathComponent:@"temp"] URLByAppendingPathExtension:@"png"];
+    CGImageDestinationRef destination = CGImageDestinationCreateWithURL((__bridge CFURLRef) tmpURL, UTI, 1, NULL);
+    CGImageDestinationAddImageFromSource(destination, source, 0, (__bridge CFDictionaryRef)imageMeta);
+    CGImageDestinationFinalize(destination);
+    CFRelease(source);
+    CFRelease(destination);
+    return tmpURL;
+ 
+}
 
 - (NSOperation *)generateImageWithCount:(NSUInteger)count progress:(void (^)(NSUInteger completed, NSUInteger failed))progress completion:(void (^)(void))completion {
     
     NSOperation *operation = [NSOperation new];
-    NSDateFormatter *formatter = [NSDateFormatter new];
-    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
     
-    NSDate *base = [formatter dateFromString:@"1990-01-01"];
+    NSDate *base = [_formatter dateFromString:@"1990:01:01 00:00:00"];
     NSDate *now = [NSDate date];
     uint32_t interval = [now timeIntervalSinceDate:base];
     
@@ -100,32 +129,39 @@
         NSUInteger completed = 0;
         
         NSDictionary *attrsDictionary = @{ NSFontAttributeName: [UIFont systemFontOfSize:1200.0], NSForegroundColorAttributeName: [UIColor whiteColor] };
+        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc]init] ;
+        [paragraphStyle setAlignment:NSTextAlignmentRight];
+        NSDictionary *dateStringAttributes = @{ NSFontAttributeName: [UIFont systemFontOfSize:120.0], NSForegroundColorAttributeName: [UIColor whiteColor], NSParagraphStyleAttributeName: paragraphStyle};
         while (completed != count && !operation.isCancelled) {
-            CGSize size = (arc4random_uniform(2) == 0) ? CGSizeMake(1280, 1960) : CGSizeMake(1960, 1280);
-            UIGraphicsBeginImageContextWithOptions(size, YES, 0);
-//            [[UIColor colorWithHue:(arc4random_uniform(1000) + arc4random_uniform(1000))/2000.0 saturation:1 brightness:1 alpha:1] setFill];
-            [[UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1] setFill];
-            CGContextRef ctx = UIGraphicsGetCurrentContext();
-            CGRect rect = (CGRect){0, 0, size};
-            CGContextFillRect(ctx, rect);
-            
-            CGRect r = CGRectMake(rect.origin.x,
-                                  rect.origin.y + (rect.size.height - size.height)/2.0,
-                                  rect.size.width,
-                                  size.height);
-            [[NSString stringWithFormat:@"%d", (int)completed] drawInRect:r withAttributes:attrsDictionary];
-            UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-            UIGraphicsEndImageContext();
-            
-            NSDate *creationDate = [base dateByAddingTimeInterval:arc4random_uniform(interval)];
-            NSError *error = nil;
-            if ([[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
-                PHAssetChangeRequest *request = [PHAssetChangeRequest creationRequestForAssetFromImage:image];
-                request.creationDate = creationDate;
-            } error:&error]) {
-                completed++;
-            } else {
-                failed++;
+            @autoreleasepool {
+                CGSize size = (arc4random_uniform(2) == 0) ? CGSizeMake(1280, 1960) : CGSizeMake(1960, 1280);
+                UIGraphicsBeginImageContextWithOptions(size, YES, 0);
+                [[UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1] setFill];
+                NSDate *takenDate = [base dateByAddingTimeInterval:arc4random_uniform(interval)];
+                
+                CGContextRef ctx = UIGraphicsGetCurrentContext();
+                CGRect rect = (CGRect){0, 0, size};
+                CGContextFillRect(ctx, rect);
+                
+                CGRect r = CGRectMake(rect.origin.x,
+                                      rect.origin.y + (rect.size.height - size.height)/2.0,
+                                      rect.size.width,
+                                      size.height);
+                CGRect dateRect = CGRectMake(rect.origin.x, rect.origin.y / 2.0, rect.size.width, rect.size.height / 2.0);
+                [[NSString stringWithFormat:@"%d", (int)completed] drawInRect:r withAttributes:attrsDictionary];
+                [[_formatter stringFromDate:takenDate] drawInRect:dateRect withAttributes:dateStringAttributes];
+                UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+                UIGraphicsEndImageContext();
+                
+                NSURL *imageURL = [self replaceTakenDateOfImage:image withDate:takenDate];
+                NSError *error = nil;
+                if ([[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+                    [PHAssetChangeRequest creationRequestForAssetFromImageAtFileURL:imageURL];
+                } error:&error]) {
+                    completed++;
+                } else {
+                    failed++;
+                }
             }
             dispatch_async(dispatch_get_main_queue(), ^{
                 progress(completed, failed);
--- PhotosGenerator/ViewController.m
+++ PhotosGenerator/ViewController.m
@@ -16,14 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"0.1.0";
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
